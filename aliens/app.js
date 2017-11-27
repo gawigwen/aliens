ebb55973b45dbada169f4663157d0a17c3bc8bd7
gawigwen <31554452+gawigwen@users.noreply.github.com>
2017-11-27 11:10:34 -1000

    Add files via upload

--- aliens/app.js
+++ aliens/app.js
@@ -1,7 +1,9 @@
 var r = 1000
 var g = 8000
 var b = 1000
-var x = 1000
+var x = 5000
+var y = 7000
+var z = 3000
 
 function totalaliens(red, green, blue){
 	return red + green + blue;
@@ -9,14 +11,27 @@ function totalaliens(red, green, blue){
 var total = totalaliens(r,g,b)
 console.log(total)
 
-function aliens(red, green, blue){
-	if(x > green){
+function greenaliens(red, green, blue){
+	if(x < green){
 		return "MOTHER OF GOD, not the green ones"
-	}else if(x > (red+green)){
+	}
+}
+var rip = greenaliens(r, g, b)
+console.log(rip)
+
+function redgreen(red, green, blue){
+	if (y < (red + green)){
 		return "DOOM!"
-	}else if(x < (blue+red)){
+	}
+}
+
+var rekt = redgreen(r, g, b)
+console.log(rekt)
+
+function bluered(red, green, blue){
+	if (z > (blue + red)){
 		return "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!"
 	}
 }
-var why = aliens(r, g, b)
-console.log(why)
+var ez = bluered(r, g, b)
+console.log(ez)
